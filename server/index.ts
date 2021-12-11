b02f272c9255f842368c15ee82f98101b762a763
Lothering0 <danil2005232@gmail.com>
2021-12-11 15:17:21 +0300

    Changed cors origin

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,7 @@ const http = require('http')
 const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://nuxt-chat0.herokuapp.com',
     methods: ['GET', 'POST']
   }
 })
