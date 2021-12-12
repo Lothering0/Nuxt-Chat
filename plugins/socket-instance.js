4f111c30f75a6f2756e48f3f5ae89bfcb49f2df1
Lothering0 <danil2005232@gmail.com>
2021-12-12 12:48:46 +0300

    Changed 'http' to 'https' on uri

--- plugins/socket-instance.js
+++ plugins/socket-instance.js
@@ -2,6 +2,6 @@ import { io } from 'socket.io-client'
 
 const uri = process.env.NODE_ENV === 'development'
   ? 'http://localhost:9000'
-  : 'http://0.22.0.1:9000'
+  : 'https://0.0.0.0:9000'
 
 export default io(uri)
