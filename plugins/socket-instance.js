50326e895a43f75ebaef99f28872bbff98d38dea
Lothering0 <danil2005232@gmail.com>
2021-12-13 13:49:06 +0300

    Changed uri when mode is production

--- plugins/socket-instance.js
+++ plugins/socket-instance.js
@@ -2,6 +2,6 @@ import { io } from 'socket.io-client'
 
 const uri = process.env.NODE_ENV === 'development'
   ? 'http://localhost:9000'
-  : 'ws://nuxt-chat0.herokuapp.com:9000'
+  : `http://${process.env.HOST}:9000`
 
 export default io(uri)
