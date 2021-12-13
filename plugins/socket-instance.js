d856f04d6dd2b16d689fab12a0f351d793ceaae5
Lothering0 <danil2005232@gmail.com>
2021-12-13 15:12:31 +0300

    Changed uri

--- plugins/socket-instance.js
+++ plugins/socket-instance.js
@@ -2,6 +2,6 @@ import { io } from 'socket.io-client'
 
 const uri = process.env.NODE_ENV === 'development'
   ? 'http://localhost:9000'
-  : `http://${process.env.HOST}:9000`
+  : 'https://nuxt-chat0-server.herokuapp.com'
 
 export default io(uri)
