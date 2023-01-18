f9a282b13a90dce93780ea8e394a1aa8f244b433
Lothering0 <88969057+Lothering0@users.noreply.github.com>
2023-01-18 13:52:22 +0300

    Debug

--- plugins/socket-instance.js
+++ plugins/socket-instance.js
@@ -4,4 +4,7 @@ const uri = process.env.NODE_ENV === 'development'
   ? 'http://localhost:9000'
   : process.env.BACKEND_ADDRESS
 
+console.log(uri)
+console.log(process.env.BACKEND_ADDRESS)
+
 export default io(uri)
