a1d55e0ee0328117ec5c5f64f2e429e8053bc8d7
Lothering0 <88969057+Lothering0@users.noreply.github.com>
2023-01-18 13:37:33 +0300

    Changed address to backend server

--- plugins/socket-instance.js
+++ plugins/socket-instance.js
@@ -2,6 +2,6 @@ import { io } from 'socket.io-client'
 
 const uri = process.env.NODE_ENV === 'development'
   ? 'http://localhost:9000'
-  : 'https://nuxt-chat0-server.herokuapp.com'
+  : process.env.BACKEND_ADDRESS
 
 export default io(uri)
